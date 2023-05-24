7998f843dcb02ba2dda0a6be8aeb823b6eba537a
oswaldo olea <o.olea@sintraconsulting.com>
2023-05-24 11:33:17 +0200

    First commit

---
+++ lib/api/users_api.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_future_builder/model/user.dart';
+import 'package:http/http.dart' as http;
+
+class UsersApi {
+  static Future<List<User>> getUsersLocally(BuildContext context) async {
+    final assetBundle = DefaultAssetBundle.of(context);
+    final data = await assetBundle.loadString('assets/users.json');
+    final body = json.decode(data);
+
+    return body.map<User>(User.fromJson).toList(); 
+  }
+}
---
+++ lib/page/user_local_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_future_builder/api/users_api.dart';
+import 'package:flutter_future_builder/model/user.dart';
+
+
+class UserLocalPage extends StatelessWidget {
+  const UserLocalPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: FutureBuilder<List<User>>(
+      future: UsersApi.getUsersLocally(context),
+      builder: (context, snapshot) {
+        final users = snapshot.data;
+
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const Center(child: CircularProgressIndicator(),);
+          default:
+            if(snapshot.hasError) {
+              return const Center(child: Text('Some error ocurred!'),);
+            } else {
+              return buildUsers(users!);
+            }
+        }
+      },
+    ),
+  );
+
+  Widget buildUsers(List<User> users) => ListView.builder(
+    physics: const BouncingScrollPhysics(),
+    itemCount: users.length,
+    itemBuilder: (context, index) {
+      final user = users[index];
+
+      return ListTile(
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(user.urlAvatar),
+        ),
+        title: Text(user.username),
+        subtitle: Text(user.email),
+      );
+    },
+  );
+}
